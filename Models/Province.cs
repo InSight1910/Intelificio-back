b1f7564a89667c01cb45c09e9a0d92c8a6feadfd
FabiJack <fabianjaque.v@duocuc.cl>
2024-08-28 23:03:39 -0400

    Creacion primeros modelos

--- Models/Province.cs
+++ Models/Province.cs
@@ -1,6 +1,8 @@
-﻿namespace Intelificio_Back.Models
+﻿using Intelificio_Back.Models.Base;
+
+namespace Intelificio_Back.Models
 {
-    public class Province
+    public class Province : BaseEntity
     {
         public string Name { get; set; }
     }
--- Models/Unit.cs
+++ Models/Unit.cs
@@ -1,6 +1,14 @@
-﻿namespace Intelificio_Back.Models
+﻿using Intelificio_Back.Models.Base;
+
+namespace Intelificio_Back.Models
 {
-    public class Unit
+    public class Unit : BaseEntity
     {
+        //public UnitType Type { get; set; }
+        public int Number { get; set; }
+        //public int UserId { get; set; }
+        //public UnitState State { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }
