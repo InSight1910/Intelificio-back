c33dd5bde14e6d6e62089bc801e538c36a1e8302
Vicente Espinosa <vice.espinosa@duocuc.cl>
2024-08-28 23:40:21 -0400

    Creados bases de modelos

---
+++ Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿using Intelificio_Back.Models.Base;
+
+namespace Intelificio_Back.Models
+{
+    public class Payment : BaseEntity
+    {
+        public int Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public string URL { get; set; }
+    }
+}
