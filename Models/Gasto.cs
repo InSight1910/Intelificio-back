b86674310b1fe6e19030803019e62745aa940eb5
Jorge Sandoval <jor.sandoval@duocuc.cl>
2024-08-28 23:39:08 -0400

    Creación de modelos asignados a Jorge

---
+++ Models/Gasto.cs
@@ -0,0 +1,12 @@
+﻿using Intelificio_Back.Models.Base;
+
+namespace Intelificio_Back.Models
+{
+    public class Gasto : BaseEntity
+    {
+        // Falta ID_Comunidad
+        // Falta ID_Tipo_Gasto
+        public int amount {  get; set; }
+        public DateTime date { get; set; }
+    }
+}
---
+++ Models/ShiftType.cs
@@ -0,0 +1,9 @@
+﻿using Intelificio_Back.Models.Base;
+
+namespace Intelificio_Back.Models
+{
+    public class ShiftType : BaseEntity
+    {
+        public string Description { get; set; }
+    }
+}
