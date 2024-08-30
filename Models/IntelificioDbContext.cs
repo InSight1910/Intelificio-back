ca18498f95a604c863baa2fb74172a49a364db64
Jorge Sandoval <jor.sandoval@duocuc.cl>
2024-08-29 22:43:19 -0400

    Actualización de IntelificioDbContext, se agregan modelos según distribución Fabian/Jorge

--- Models/IntelificioDbContext.cs
+++ Models/IntelificioDbContext.cs
@@ -10,6 +10,24 @@ namespace Intelificio_Back.Models
         public DbSet<AssignedShift> AssignedShifts { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
 
+        public DbSet<Visit> Visits { get; set; }
+
+        public DbSet<UnitType> UnitTypes { get; set; }
+
+        public DbSet<Unit> Units { get; set; }
+
+        public DbSet<ShiftType> ShiftTypes { get; set; }
+
+        public DbSet<Shift> Shifts { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Province> Provinces { get; set; }
+
+        public DbSet<Pet> Pets { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
